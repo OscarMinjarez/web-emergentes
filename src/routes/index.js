b6f79bd7138dd61a9f45a79bfa8d949f9b27274a
OscarMinjarez <oscarminjarez@outlook.com>
2023-10-11 00:16:19 -0700

    Se creó un archivo de rutas y se agregaron rutas

---
+++ src/routes/index.js
@@ -0,0 +1,29 @@
+const { Router } = require("express");
+const IngredientesController = require("../controllers/ingrediente-controller");
+
+const router = new Router();
+const ingredientesController = new IngredientesController();
+
+router.get("/", (req, res) => res.send("Hello World"));
+
+router.get("/ingredientes/:id", async (req, res) => {
+    await ingredientesController.obtenerPorId(req, res);
+});
+
+router.get("/ingredientes", async (req, res) => {
+    await ingredientesController.obtenerTodos(req, res);
+});
+
+router.post("/ingredientes", async (req, res) => {
+    await ingredientesController.crear(req, res);
+});
+
+router.patch("/ingredientes/:id", async (req, res) => {
+    await ingredientesController.actualizar(req, res);
+});
+
+router.delete("/ingredientes/:id", async (req, res) => {
+    await ingredientesController.eliminar(req, res);
+});
+
+module.exports = router;
