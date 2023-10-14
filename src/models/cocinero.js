f8349f4f9e59c38b2def52c29ba7f4cdde98b954
SopitasWick <147137063+SopitasWick@users.noreply.github.com>
2023-10-13 22:49:20 -0700

    Create cocinero.js
    Modelo para la entidad cocinero

---
+++ src/models/cocinero.js
@@ -0,0 +1,9 @@
+class Cocinero{
+ constructor(id, nombreUsuario, contrasenia, superUsuarioId) {
+        this.id = id;
+        this.nombreUsuario=nombreUsuario;
+        this.contrasenia=contrasenia;
+        this.superUsuarioId=superUsuarioId;
+    }
+}
+module.exports = Cocinero;
