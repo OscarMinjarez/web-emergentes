e13ead7dfbff17835039dba9f90c157b540558fb
OscarMinjarez <oscarminjarez@outlook.com>
2023-11-03 17:26:47 -0700

    Se creó un archivo env para variables de entorno y un archivo para probar peticiones

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const routes = require("./src/routes/index");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 
 app.listen(3000, () => {
